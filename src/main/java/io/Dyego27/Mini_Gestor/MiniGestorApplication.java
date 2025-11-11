1f1bdfb466dcaa8df73f0008a3785ea102ac17d7
Dyego27 <dyegoluis123@gmail.com>
2025-11-11 18:53:46 -0300

    Criação da  hierarquia de pastas

---
+++ src/main/java/io/Dyego27/Mini_Gestor/MiniGestorApplication.java
@@ -0,0 +1,13 @@
+package io.Dyego27.Mini_Gestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniGestorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniGestorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/Dyego27/Mini_Gestor/MiniGestorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.Dyego27.Mini_Gestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniGestorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
